a696a5d912b73d45e2023ab769bd71b2e219822f
Dennis <nndenninn@gmail.com>
2020-05-10 21:23:08 +0300

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Dennis Nesanoff on 10.05.2020.
+//  Copyright © 2020 Dennis Nesanoff. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
